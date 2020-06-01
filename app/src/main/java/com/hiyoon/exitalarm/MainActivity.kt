07cd7a43f4855ff92a6474d73c43977ce6777da5
11102 <dev.hiyoon@gmail.com>
2020-06-01 17:43:55 +0900

    init

--- app/src/main/java/com/hiyoon/exitalarm/MainActivity.kt
+++ app/src/main/java/com/hiyoon/exitalarm/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.hiyoon.exitalarm
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // set kakao map
+         
     }
+
+
+    /*
+    fun getKeyHash(context: Context?): String? {
+        try {
+            val info =
+                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures) {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.e(
+                    "MY KEY HASH:",
+                    Base64.encodeToString(md.digest(), Base64.DEFAULT)
+                )
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+        } catch (e: NoSuchAlgorithmException) {
+        }
+
+        return null
+    }
+
+     */
 }
