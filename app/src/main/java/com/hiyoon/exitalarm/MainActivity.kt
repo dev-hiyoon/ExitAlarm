69cc9d37c44c1382cdc903d2b831254c397ef039
11102 <dev.hiyoon@gmail.com>
2020-07-24 18:26:49 +0900

    다음지도 jar 파일 추가

--- app/src/main/java/com/hiyoon/exitalarm/MainActivity.kt
+++ app/src/main/java/com/hiyoon/exitalarm/MainActivity.kt
@@ -1,46 +1,52 @@
 package com.hiyoon.exitalarm
 
-import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Base64
 import android.util.Log
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
+import kotlinx.android.synthetic.main.activity_main.*
+import net.daum.mf.map.api.MapView
 
 
 class MainActivity : AppCompatActivity() {
+
+
+    private val PERMISSIONS_REQUEST_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         // set kakao map
-         
+        val mapView = MapView(this)
+        map_view.addView(mapView)
     }
 
+    // https://webnautes.tistory.com/1319
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        Log.d("exitalarm.test", "########################### hiyoon")
 
-    /*
-    fun getKeyHash(context: Context?): String? {
-        try {
-            val info =
-                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
-            for (signature in info.signatures) {
-                val md = MessageDigest.getInstance("SHA")
-                md.update(signature.toByteArray())
-                Log.e(
-                    "MY KEY HASH:",
-                    Base64.encodeToString(md.digest(), Base64.DEFAULT)
-                )
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                } else {
+                    // Explain to the user that the feature is unavailable because
+                    // the features requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+                }
+
+                return
+            } else -> {
+                // Ignore all other requests.
             }
-        } catch (e: PackageManager.NameNotFoundException) {
-        } catch (e: NoSuchAlgorithmException) {
         }
-
-        return null
     }
-
-     */
 }
