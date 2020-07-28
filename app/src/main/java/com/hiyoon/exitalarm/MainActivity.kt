fe375b0d8bdc65500b47eff9aad2d33b69ea1bfd
11102 <dev.hiyoon@gmail.com>
2020-07-28 10:43:26 +0900

    권한확인 추가

--- app/src/main/java/com/hiyoon/exitalarm/MainActivity.kt
+++ app/src/main/java/com/hiyoon/exitalarm/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.hiyoon.exitalarm
 
+import android.Manifest
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import net.daum.mf.map.api.MapView
@@ -12,7 +15,7 @@ import net.daum.mf.map.api.MapView
 class MainActivity : AppCompatActivity() {
 
 
-    private val PERMISSIONS_REQUEST_CODE = 100
+    private val PERMISSION_REQUEST_CODE = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,31 +24,50 @@ class MainActivity : AppCompatActivity() {
         // set kakao map
         val mapView = MapView(this)
         map_view.addView(mapView)
+
+        // check pemission
+        requestPermissions(
+            arrayOf(
+                Manifest.permission.INTERNET,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ), // 1
+            PERMISSION_REQUEST_CODE
+        ) // 2
     }
 
-    // https://webnautes.tistory.com/1319
-    override fun onRequestPermissionsResult(requestCode: Int,
-                                            permissions: Array<String>, grantResults: IntArray) {
-        Log.d("exitalarm.test", "########################### hiyoon")
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
 
         when (requestCode) {
-            PERMISSIONS_REQUEST_CODE -> {
-                // If request is cancelled, the result arrays are empty.
-                if ((grantResults.isNotEmpty() &&
-                            grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    // Permission is granted. Continue the action or workflow
-                    // in your app.
-                } else {
-                    // Explain to the user that the feature is unavailable because
-                    // the features requires a permission that the user has denied.
-                    // At the same time, respect the user's decision. Don't link to
-                    // system settings in an effort to convince the user to change
-                    // their decision.
+            PERMISSION_REQUEST_CODE -> {  // 1
+                if (grantResults.isEmpty()) {  // 2
+                    throw RuntimeException("Empty permission result")
                 }
 
-                return
-            } else -> {
-                // Ignore all other requests.
+                // check grant result
+                grantResults.forEach {
+                    if (it == PackageManager.PERMISSION_GRANTED) {  // 3
+                        Toast.makeText(applicationContext, "Permission granted", Toast.LENGTH_LONG)
+                    } else {
+                        if (shouldShowRequestPermissionRationale(
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                            )
+                        ) { // 4
+                            requestPermissions(
+                                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                                PERMISSION_REQUEST_CODE
+                            )
+                        } else {
+                            Toast.makeText(
+                                applicationContext,
+                                "User declined and i can't ask",
+                                Toast.LENGTH_LONG
+                            )
+                        }
+                    }
+                }
             }
         }
     }
